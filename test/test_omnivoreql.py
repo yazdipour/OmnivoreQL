d1079f467a2cd1eec4cf2841c4f331c09664b7ae
Shahriar <yazdipour@users.noreply.github.com>
2023-07-02 13:21:22 +0200

    fix: test to use static client

--- test/test_omnivoreql.py
+++ test/test_omnivoreql.py
@@ -5,20 +5,21 @@ from dotenv import load_dotenv
 
 # Add the path to the folder containing the omnivoreql module to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import omnivoreql.omnivoreql as omnivoreql
+from omnivoreql.omnivoreql import OmnivoreQL
 
 class TestOmnivoreQL(unittest.TestCase):
-    omnivoreql = None
+    client = None
 
-    def setUp(self):
-        if self.omnivoreql is None:
+    @classmethod
+    def setUpClass(cls):
+        if cls.client is None:
             # Put in .env file or use 'python -m unittest test_omnivoreql.py OMNIVORE_API_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-            self.api_token = self.getEnvVariable('OMNIVORE_API_TOKEN')
-            print("OMNIVORE_API_TOKEN: " + self.api_token[:4])
-            self.omnivoreql = omnivoreql.OmnivoreQL(
-                "https://api-prod.omnivore.app/api/graphql", self.api_token)
+            api_token = cls.getEnvVariable('OMNIVORE_API_TOKEN')
+            print("OMNIVORE_API_TOKEN: " + api_token[:4])
+            cls.client = OmnivoreQL(api_token)
 
-    def getEnvVariable(self, variable_name):
+    @staticmethod
+    def getEnvVariable(variable_name):
         for arg in sys.argv[1:]:
             if arg.startswith(variable_name + '='):
                 return arg.split('=')[1]
@@ -27,35 +28,35 @@ class TestOmnivoreQL(unittest.TestCase):
         return os.environ.get(variable_name)
 
     def test_get_profile(self):
-        profile = self.omnivoreql.get_profile()
+        profile = self.client.get_profile()
         self.assertIsNotNone(profile)
 
     def test_save_url(self):
-        result = self.omnivoreql.save_url("https://www.google.com")
+        result = self.client.save_url("https://www.google.com")
         self.assertIsNotNone(result)
         self.assertFalse('errorCodes' in result['saveUrl'])
 
     def test_get_articles(self):
-        articles = self.omnivoreql.get_articles()
+        articles = self.client.get_articles()
         self.assertIsNotNone(articles)
         self.assertFalse('errorCodes' in articles['search'])
 
     def test_get_article(self):
-        username = self.omnivoreql.get_profile()['me']['profile']['username']
-        slug = self.omnivoreql.get_articles()['search']['edges'][0]['node']['slug']
-        articles = self.omnivoreql.get_article(
+        username = self.client.get_profile()['me']['profile']['username']
+        slug = self.client.get_articles()['search']['edges'][0]['node']['slug']
+        articles = self.client.get_article(
             username, slug
         )
         self.assertIsNotNone(articles)
         self.assertFalse('errorCodes' in articles['article'])
 
     def test_get_labels(self):
-        labels = self.omnivoreql.get_labels()
+        labels = self.client.get_labels()
         self.assertIsNotNone(labels)
         self.assertFalse('errorCodes' in labels['labels'])
 
     def test_get_subscriptions(self):
-        subscriptions = self.omnivoreql.get_subscriptions()
+        subscriptions = self.client.get_subscriptions()
         self.assertIsNotNone(subscriptions)
         self.assertFalse('errorCodes' in subscriptions['subscriptions'])
 
