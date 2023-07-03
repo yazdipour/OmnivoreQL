96e0c8132982972d2c3d79005ff6713764efc27e
Shahriar <yazdipour@users.noreply.github.com>
2023-07-03 21:15:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 from setuptools import setup, find_packages
+import subprocess
 
-VERSION = "0.2.3.2"
+def get_latest_git_tag():
+    try:
+        version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        version = version.strip().decode("utf-8")  # Remove trailing newline and decode bytes to string
+
+        # Remove the 'v' from the tag
+        if version.startswith('v'):
+            version = version[1:]
+
+        return version
+    except Exception as e:
+        print(f"An exception occurred while getting the latest git tag: {e}")
+        return None
+
+VERSION = get_latest_git_tag() or "0.0.1"  # Fallback version
+
 PROJECT_URLS = {
     "Bug Tracker": "https://github.com/yazdipour/OmnivoreQL/issues",
     "Source Code": "https://github.com/yazdipour/OmnivoreQL",
