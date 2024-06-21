f1c139e0fe274ec39294c06652d2c19c99832450
Shahriar <yazdipour@users.noreply.github.com>
2024-06-22 01:00:44 +0200

    fix: Update pydoc (#26)
    * fix: Update pydoc
    
    Add article limit for get_articles method
    
    * fix: add default limit for get_articles

--- omnivoreql/omnivoreql.py
+++ omnivoreql/omnivoreql.py
@@ -110,9 +110,9 @@ class OmnivoreQL:
         include_content: bool = False,
     ):
         """
-        Get articles for the current user.
+        Get articles for the current user. It is limited to first 100 articles by default. Use limit and cursor to fetch more.
 
-        :param limit: The number of articles to return.
+        :param limit: The number of articles to return (default is 100).
         :param cursor: The cursor to use for pagination.
         :param format: The output format of the articles. Can be 'html' (default) or 'markdown'.
         :param query: The query to use for filtering articles. Example of query by date: 'in:inbox published:2024-03-01..*'. See https://docs.omnivore.app/using/search.html#filtering-by-save-publish-dates for more information.
